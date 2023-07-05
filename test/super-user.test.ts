7e11b80bed0d0025f03d04de37654faecf347c60
Darren Harris <dazza@me.com>
2023-07-05 10:31:32 +0800

    Initial commit

---
+++ test/super-user.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SuperUser from '../lib/super-user-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/super-user-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SuperUser.SuperUserStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
