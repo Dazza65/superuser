22d4e70f1ddd1ef224a2a3160f789f43263d3a04
Darren Harris <dazza@me.com>
2023-08-05 18:30:14 +0800

    fix: correct switch-role script

--- scripts/switch-role.sh
+++ scripts/switch-role.sh
@@ -1,14 +1,14 @@
-for keyval in $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
+for keyval in $(printf "AWS_ACCESS_KEY_ID:%s AWS_SECRET_ACCESS_KEY:%s AWS_SESSION_TOKEN:%s" \
     $(aws sts assume-role \
-        --role-arn arn:aws:iam::887312641336:role/applicationroles/TestLambdaRole \
+        --role-arn arn:aws:iam::887312641336:role/applicationroles/superuser/SuperUserRole \
         --role-session-name MySessionName \
         --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
     --output text))
 do
-    key=`echo $keyval | cut -d= -f1`
-    val=`echo $keyval | cut -d= -f2 | tr '[:upper:]' '[:lower:]'`
+    key=`echo $keyval | cut -d: -f1 | tr '[:upper:]' '[:lower:]'`
+    val=`echo $keyval | cut -d: -f2`
 
-    aws configure set `echo ${key} ` ${val} --profile assumed-role
+    aws configure set ${key} ${val} --profile assumed-role
 done
 
 aws configure set region ap-southeast-2 --profile assumed-role
