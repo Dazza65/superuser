751083520d7c0e3ff1076cf7f00b8cc201cee70b
Darren Harris <dazza@me.com>
2023-08-05 18:31:24 +0800

    feat: modify to allow role creation with the CDK bootstrapped permission boundary

--- lib/super-user-stack.ts
+++ lib/super-user-stack.ts
@@ -1,15 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
-import { AccountRootPrincipal, ManagedPolicy, Role } from 'aws-cdk-lib/aws-iam';
+import { AccountRootPrincipal, ManagedPolicy, Policy, Role } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SuperUserStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const permissionsBoundary = ManagedPolicy.fromManagedPolicyArn(this, 'devBoundaryPolicy', 'arn:aws:iam::887312641336:policy/DeveloperBoundaryPolicy');
+
+    console.log(`BP: ${permissionsBoundary}`);
+    
     new Role(this, 'SuperUserRole', {
       roleName: 'SuperUserRole',
       assumedBy: new AccountRootPrincipal(),
+      path: '/applicationroles/superuser/',
+      permissionsBoundary,
       managedPolicies: [
         ManagedPolicy.fromManagedPolicyArn(this, 'AdminPolicy', 'arn:aws:iam::aws:policy/AdministratorAccess')
       ]
